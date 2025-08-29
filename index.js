57b116daa7f2937c5c39f1776a30e929bfcc4167
Raghul <raghulraghu2002@gmail.com>
2025-08-30 05:04:45 +0800

    second commit

--- index.js
+++ index.js
@@ -4,23 +4,38 @@ const io = new Server(4000, {
   cors: { origin: "*" },
 });
 
+let locks = {}; //stores data of locked indexes
+let latestData = []; // stores data on every line updates
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
-  socket.on("line_locked", (data) => {
-    io.emit("line_locked", data); // broadcast to all
-  });
-     socket.on("live_updates", (data) => {
-        io.emit("live_updates", data); // broadcast to all
-       
-      });
+  io.emit("updated_lines", latestData); // sending the latest data when user login
 
-  socket.on("line_unlocked", (data) => {
-    io.emit("line_unlocked", data);
-  }); 
+  socket.on("line_locked", ({ lineIndex, username }) => {
+    locks[socket.id] = lineIndex; // { D6O9u8TsDZQQdWc2AAAS: 1 }
+
+    io.emit("line_locked", { lineIndex, username });
+  });
+
+  socket.on("live_updates", (data) => {
+    latestData[0] = data.newLines;
+    io.emit("live_updates", data); //live line updates
+  });
+
+  socket.on("line_unlocked", ({ lineIndex }) => {
+    if (locks[socket.id] === lineIndex) {
+      delete locks[socket.id]; // Removing the user and index from locks object
+    }
+    io.emit("line_unlocked", { lineIndex });
+  });
 
   socket.on("disconnect", () => {
     console.log("User disconnected:", socket.id);
+
+    if (locks[socket.id] !== undefined) {
+      io.emit("line_unlocked", { lineIndex: locks[socket.id] }); // Triggering unlock incase user got disconnected
+      delete locks[socket.id];
+    }
   });
 });
 
